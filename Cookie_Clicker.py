355bdfc6d56241c3834d49eade09da7273ab119b
sgtdiddlywink <96023725+sgtdiddlywink@users.noreply.github.com>
2022-09-23 15:06:16 -0400

    Update Cookie_Clicker.py

--- Cookie_Clicker.py
+++ Cookie_Clicker.py
@@ -5,7 +5,7 @@ import time
 
 """Need to install the appropriate browser driver and place .exe in accessible file."""
 # Chrome driver path should reference the .exe browser driver.
-chrome_driver_path = "E:\Py Code\Development\chromedriver.exe"
+chrome_driver_path = "NEED TO ADD YOUR OWN PATH TO YOUR BROWSER EXE DRIVER"
 
 # Access the driver
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
